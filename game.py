cd1030661a41ef145bf1df5258d2bcab7414e511
Inha Bae <baeinha030603@gmail.com>
2022-07-11 14:43:06 -0700

    add method to check win conditions

--- game.py
+++ game.py
@@ -1,27 +1,56 @@
 class TicTacToeGame: 
     def __init__(self):
         # Player's mark on the board
-        self.X_MARK = 1
-        self.O_MARK = 2
-        self.NO_MARK = 0
+        self.X_MARK = 'X'
+        self.O_MARK = 'O'
+        self.NO_MARK = '-'
 
         # Board
         self.board = dict()
+        
+        # Win conditions
+        self.win_con = [
+            (1,2,3), (4,5,6), (7,8,9),
+            (1,4,7), (2,5,8), (3,6,9,),
+            (1,5,9), (3,5,7)
+        ]
+
+        # Player conditions
+        self.player_con = []
 
     def print_board(self):
         print(f"|{self.board[1]} {self.board[2]} {self.board[3]}|")
         print(f"|{self.board[4]} {self.board[5]} {self.board[6]}|")
         print(f"|{self.board[7]} {self.board[8]} {self.board[9]}|")
 
+    # Create an empty board
     def init_board(self):
         for i in range(9):
             self.board[i+1] =  self.NO_MARK
 
-    def place_mark(self):
+    def show_available_squares(self):
         available_squares = []
-        for i in len(self.board):
-            if self.board[i+1] != self.NO_MARKS:
-                available_squares.append(i+1)
-# Create an empty board
+        for i in self.board.keys():
+            if self.board[i] is self.NO_MARK:
+                available_squares.append(i)
+        print("Availabe squares are: ")
+        print(available_squares)
+
+    def mark_square(self, input):
+        if type(input) != int:
+            print("Please enter a valid integer.")
+        self.board[input] = self.X_MARK
+        self.player_con.append(input)
+
+    def check_win(self):
+        for con in self.win_con:
+            win_con_checker = 0
+            for square in con:
+                if square not in self.player_con:
+                    win_con_checker += 1
+            if win_con_checker == 0:
+                return True
+        
+        return False
 
 
--- game_test.py
+++ game_test.py
@@ -3,11 +3,46 @@ from game import TicTacToeGame
 tg = TicTacToeGame()
 
 # Testing init_board()
+print("Testing init_board():")
 tg.init_board()
 print("Expected:")
-print("|0 0 0|")
-print("|0 0 0|")
-print("|0 0 0|")
+print("|- - -|")
+print("|- - -|")
+print("|- - -|")
 
 print("Actual:")
 tg.print_board()
+
+# Testing show_available_squares()
+print("\nTesting show_available_squares():")
+print("All 9 squares should be available, as no marks have been placed.")
+tg.show_available_squares()
+
+# Testing mark_sqaure()
+print("\nTesting mark_sqaure():")
+print("After marking the square 3, there should be a X mark.")
+tg.mark_square(3)
+tg.print_board()
+print("Now, there should be 8 available squares, 1-9 except 3.")
+tg.show_available_squares()
+
+# Testing check_win()
+print("\nTesting check_win():")
+print("Right now, no players have won, thus the game should continue.")
+print("Expected:\nThe game continues.")
+print("Actual:")
+if tg.check_win():
+    print("The game is over, someone won!")
+else:
+    print("The game continues.")
+
+tg.mark_square(5)
+tg.mark_square(7)
+print("After placing X marks on square 5 and 7, the game should be over.")
+tg.print_board()
+print("Expected:\nThe game is over, someone won!")
+print("Actual:")
+if tg.check_win():
+    print("The game is over, someone won!")
+else:
+    print("The game continues.")
