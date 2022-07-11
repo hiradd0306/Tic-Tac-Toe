91db72b752c19db84b1a8b4d0f8d339d1b8a12b6
Inha Bae <baeinha030603@gmail.com>
2022-07-11 15:55:21 -0700

    Add more readability

--- simulator.py
+++ simulator.py
@@ -4,27 +4,29 @@ tg = TicTacToeGame()
 
 tg.init_board()
 while True:
-    tg.show_turn()
     tg.show_available_squares()
+    tg.print_board()
     if tg.game_over:
         break
 
     while True:
         try: 
-            square = int(input("Choose a square: "))
+            tg.show_turn()
+            square = int(input("Choose a square number: "))
             if square not in tg.available_squares:
                 raise ValueError
         except ValueError:
             print("Please enter a valid input.\n")
+            tg.print_board()
         else:
             break
 
     tg.mark_square(square)
-    tg.print_board()
     print("")
     tg.check_win()
     if tg.game_over:
         winner = 'X' if tg.turn==0 else 'O'
+        tg.print_board()
         print(f"GG, {winner} wins!!") 
         break
     tg.swap_turn()
